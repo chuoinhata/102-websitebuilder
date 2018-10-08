6ded7026091525ea604364f09ee1513b78aa9c19
Tri <nvtri.dtu@gmail.com>
2018-10-08 19:40:52 +0700

    Style header

--- wp-content/themes/photomart/resources/views/partials/header.blade.php
+++ wp-content/themes/photomart/resources/views/partials/header.blade.php
@@ -1,10 +1,156 @@
-<header class="banner">
-  <div class="container">
-    <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
-    <nav class="nav-primary">
-      @if (has_nav_menu('primary_navigation'))
-        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
-      @endif
-    </nav>
-  </div>
+<header class="header-warpper">
+	<div class="container">
+		<div class="header-inner">
+			<div class="row">
+				<div class="col-xs-12 col-sm-12 col-md-2 col-lg-2 header-logo">
+					<h1 class="logo">
+						<a href="https://www.photomart.com.au/" title="Photomart" class="logo">
+							<img src="@asset( 'images/logo.png' )" alt="Photomart">
+						</a>
+					</h1>
+				</div>
+
+				<div class="col-xs-12 col-sm-12 col-md-10 col-lg-10 header-menu">
+					<nav class="navbar navbar-expand-lg">
+						<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+							<span class="navbar-toggler-icon"></span>
+						</button>
+
+						<div class="collapse navbar-collapse" id="navbarSupportedContent">
+							<ul class="navbar-nav">
+								<li class="nav-item dropdown">
+									<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+										Our PRODUCTS
+									</a>
+									<div class="mega-menu">
+										<div class="mega-menu-inner">
+											<div class="row">
+												<div class="col-md-2">
+													<a href="/framing">
+														<img src="@asset( 'images/framing.jpg' )">
+														<span>Framing</span>
+													</a>
+												</div>
+
+												<div class="col-md-2">
+													<a href="/canvas">
+														<img src="@asset( 'images/canvas.jpg' )">
+														<span>Canvas</span>
+													</a>
+												</div>
+
+												<div class="col-md-2">
+													<a href="/printing">
+														<img src="@asset( 'images/printing.jpg' )">
+														<span>Printing</span>
+													</a>
+												</div>
+
+												<div class="col-md-2">
+													<a href="/torn-edge-products">
+														<img src="@asset( 'images/torn-edge.jpg' )">
+														<span>Torn Edge</span>
+													</a>
+												</div>
+
+												<div class="col-md-2">
+													<a href="/insta-frames">
+														<img src="@asset( 'images/insta-frames-thumbnail.jpg' )">
+														<span>Insta Frames</span>
+													</a>
+												</div>
+
+												<div class="col-md-2">
+													<a href="/more-products">
+														<img src="@asset( 'images/wallpaper.jpg' )">
+														<span>More products</span>
+													</a>
+												</div>
+											</div>
+										</div>
+									</div>
+								</li>
+								<li class="nav-item">
+									<a href="tel:02 4297 0005">(02) 4297 0005</a>
+								</li>
+								<li class="nav-item">
+									<a href="#" title="Log In/Sign Up">Log In/Sign Up</a>
+								</li>
+								<li class="nav-item">
+									<div class="mini-cart">
+										<a href="javascript:void(0)" class="mybag-link">
+											<i class="icon-mini-cart"></i>
+											<span class="cart-info">
+												<span class="cart-qty">0</span>
+											</span>
+										</a>
+										<div class="topCartContent block-content theme-border-color" style="display: none;">
+											<div class="close-cart"><i class="fa fa-times" aria-hidden="true"></i></div>
+											<div class="inner-wrapper">                                                    
+												<p class="cart-empty">
+													You have no items in your shopping cart.
+												</p>
+											</div>
+										</div>
+									</div>
+								</li>
+								<li class="nav-item dropdown">
+									<a class="nav-link dropdown-toggle btn-outline" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+										<i class="fa fa-shopping-cart" aria-hidden="true"></i> ORDER HERE
+									</a>
+									<div class="mega-menu">
+										<div class="mega-menu-inner">
+											<div class="row">
+												<div class="col-md-2">
+													<a href="/framing">
+														<img src="@asset( 'images/framing.jpg' )">
+														<span>Framing</span>
+													</a>
+												</div>
+
+												<div class="col-md-2">
+													<a href="/canvas">
+														<img src="@asset( 'images/canvas.jpg' )">
+														<span>Canvas</span>
+													</a>
+												</div>
+
+												<div class="col-md-2">
+													<a href="/printing">
+														<img src="@asset( 'images/printing.jpg' )">
+														<span>Printing</span>
+													</a>
+												</div>
+
+												<div class="col-md-2">
+													<a href="/torn-edge-products">
+														<img src="@asset( 'images/torn-edge.jpg' )">
+														<span>Torn Edge</span>
+													</a>
+												</div>
+
+												<div class="col-md-2">
+													<a href="/insta-frames">
+														<img src="@asset( 'images/insta-frames-thumbnail.jpg' )">
+														<span>Insta Frames</span>
+													</a>
+												</div>
+
+												<div class="col-md-2">
+													<a href="/more-products">
+														<img src="@asset( 'images/wallpaper.jpg' )">
+														<span>More products</span>
+													</a>
+												</div>
+											</div>
+										</div>
+									</div>
+								</li>
+							</ul>
+						</div>
+					</nav>
+				</div>
+			</div>
+		</div>
+	</div>
 </header>
