a3cc92bd12ca51e522a2dac5fafeba13b1fbb15c
Tri <nvtri.dtu@gmail.com>
2018-10-08 19:40:34 +0700

    Style header

--- wp-content/themes/photomart/resources/views/partials/footer.blade.php
+++ wp-content/themes/photomart/resources/views/partials/footer.blade.php
@@ -1,5 +1,53 @@
-<footer class="content-info">
-  <div class="container">
-    @php dynamic_sidebar('sidebar-footer') @endphp
-  </div>
+<footer class="footer-warpper">
+	<div class="container">
+		<div class="row">
+			<div class="col-12 col-md-6 footer-top-left footer-widget">
+				<h3 class="title-footer">Subscribe Today</h3>
+				<p class="text-footer">Keep up to date with the latest in Printing and Framing trends. We would love to share any news or important information with you.</p>
+				<form class="form-subscribe" action="" name="subscribeForm" method="post" novalidate="novalidate">
+					<div class="input-group">
+						<input id="search" type="email" name="email" value="" class="input-text form-control" placeholder="Your email address">
+						<span class="input-group-btn">
+							<button type="submit" title="Search" class="btn btn-default"><span>Subscribe</span></button>
+						</span>
+					</div>
+				</form><!-- .form-subscribe -->
+			</div>
+			<div class="col-12 col-md-6 footer-top-right">
+				<div class="row">
+					<div class="col-12 col-sm-6 col-md-6 footer-widget">
+						<h3 class="title-footer">Quick Links</h3>
+						<ul class="menu-footer">
+							<li><a href="/framing">FRAMING</a></li>
+							<li><a href="/canvas">CANVAS</a></li>
+							<li><a href="/print">PRINTING</a></li>
+							<li><a href="/torn-edge-products">Torn Edge</a></li>
+							<li><a href="/insta-frames">INSTA FRAMES</a></li>
+							<li><a href="/more-products">MORE PRODUCTS</a></li>
+						</ul>
+					</div>
+					<div class="col-12 col-sm-6 col-md-6 footer-widget">
+						<h3 class="title-footer">Navigation</h3>
+						<ul class="menu-footer">
+							<li><a href="/">Home</a></li>
+							<li><a href="/contact">Contact</a></li>
+							<li><a href="/photo-credits">Photo Credits</a></li>
+							<li><a href="/faq">FAQ</a></li>
+							<li><a href="/about-us.html">About Us</a></li>
+							<li><a href="/term-of-trade">Term Of Trade</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </footer>
+
+<section class="footer-bottom">
+	<div class="container">
+		© 2017 - Photomart. Ltd.
+
+		<script async="" type="text/javascript" src="https://seal.godaddy.com/getSeal?sealID=N9ncacT2wMq8ROAox5cz6bJdl1s6rPX9GtFb5E2T2LtIveDdJratpckFmJxs"></script>
+		<span id="siteseal" class="godaddy-seal"></span>
+	</div>
+</section>
