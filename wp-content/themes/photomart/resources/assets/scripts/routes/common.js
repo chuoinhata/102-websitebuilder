2dd8002316ff56fa0f8b15d9304a53780af1b0e6
Tri <nvtri.dtu@gmail.com>
2018-10-08 19:39:41 +0700

    Style global

--- wp-content/themes/photomart/resources/assets/scripts/routes/common.js
+++ wp-content/themes/photomart/resources/assets/scripts/routes/common.js
@@ -1,8 +1,21 @@
 export default {
-  init() {
+	init() {
     // JavaScript to be fired on all pages
   },
   finalize() {
-    // JavaScript to be fired on all pages, after page specific JS is fired
+    $( '.mini-cart' ).mouseover( function() {
+      $( this ).children( '.topCartContent' ).fadeIn( 200 )
+      return false
+    } ).mouseleave( function() {
+      $( this ).children( '.topCartContent' ).fadeOut( 200 )
+      return false
+    } )
+
+    const header = $( '.header-warpper' )
+    $( window ).scroll( function() {    
+      var scroll = $( window ).scrollTop()
+      header.toggleClass("sticky-deactive", scroll > 10)
+      header.toggleClass("sticky-active", scroll > 60)
+    } )
   },
-};
+}
